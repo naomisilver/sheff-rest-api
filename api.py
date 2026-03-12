85cdcd3cdd71258026415eff719cebb05d11deac
naomisilver <2425281@dundee.ac.uk>
2026-03-12 23:49:31 +0000

    added alternative etl script and status endpoint
    Added an alternative etl script which invokes the REST API as apposed to direct access to the database along with a /status endpoint.

--- api.py
+++ api.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 """
     TODO:
-        - add a add error handling for 404 and 400 
+        - add error handling for 404 and 400 
 
         - mirror the tuple unpacking I did in etl.py
+            - unsure if I can/can't do tuple unpacking as I'm addressing specific
 """
 
 @app.route("/orders", methods=['GET'])
@@ -17,7 +18,6 @@ def get_items():
 
     conn = sqlite3.connect("customers.db")
     c = conn.cursor()
-    #c.execute("SELECT * FROM customers WHERE customer_id=?", (customer_id,))
     c.execute("SELECT * FROM customers LEFT JOIN orders ON customers.customer_id = orders.customer_id WHERE customers.customer_id = ?", (customer_id,))
     rows = c.fetchall()
     conn.close()
@@ -29,6 +29,34 @@ def get_items():
             customer["orders"].append({"order_id": row[5], "customer_id": row[6], "product_name": row[7], "product_barcode": row[8], "quantity": row[9], "unit_price": row[10]})
 
     return customer
+
+# copied but adapted from etl.py to serve etl-alt.py so instead of querying just for the necessary information in SQL, I query it all but structure it much the
+# same but to serve an API endpoint. Though, serving the same information as the /items endpoint, and creating an entire endpoint for it seems wasteful, this
+# be better done through checking what search param is given so instead of "orders?customer_id=1" you would do "orders?status=active" to invoke, will look into
+# this :D
+@app.route("/status", methods=['GET'])
+def get_status():
+
+    status = request.args.get("status")
+
+    conn = sqlite3.connect("customers.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM customers LEFT JOIN orders ON customers.customer_id = orders.customer_id WHERE customers.status=?", (status,))
+    rows = c.fetchall()
+    conn.close()
+
+    customers = {}
+
+    for row in rows: 
+        customer_id, first_name, family_name, email, status, order_id, order_customer_id, product_name, product_barcode, quantity, unit_price = row
+
+        if customer_id not in customers:
+            customers[customer_id] = {"customer_id": customer_id, "first_name": first_name, "family_name": family_name, "email": email, "orders": [], "status": status}
+
+        if quantity is not None:
+            customers[customer_id]["orders"].append({"order_id": order_id, "customer_id": order_customer_id, "product_name": product_name, "product_barcode": product_barcode, "quantity": quantity, "unit_price": unit_price})
+
+    return list(customers.values())
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ etl-alt.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import sys
+from pathlib import Path
+
+OUTPUT_DIR = Path("output")
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+CSV_FILE = OUTPUT_DIR / Path("output-alt.csv")
+
+API_ROOT = "http://127.0.0.1:5000/"
+
+headers = {
+        'user-agent': "sheffield-rest-api" # I'm not checking this in flask but good practice
+    }
+
+session = requests.Session() # not necessarily needed as I'm making a single call, and so maintaining the same session across invocations has little benefit
+
+def get_status(status: str):
+    
+    payload = {
+        "status": status
+    }
+
+    r = session.get(f"{API_ROOT}status", headers=headers, params=payload)
+    #print(r.status_code)
+
+    data = r.json()
+
+    return data
+
+def transform():
+    try:
+        status = sys.argv[1]
+    except IndexError:
+        print("provide a status type: 'active', 'suspended' or 'archived'")
+        raise SystemExit
+
+    customers = get_status(status)
+
+    with open(CSV_FILE, "w+", newline='') as csv_file:
+        fieldnames = "name,total order value"
+        csv_file.write(f"{fieldnames}\n")
+
+        for customer in customers:
+            order_total = 0
+
+            c_name = f"{customer['first_name']} {customer['family_name']}"
+            for order in customer["orders"]:
+                order_total = order_total + order["quantity"] * order["unit_price"]
+
+            csv_file.write(f"{c_name},{order_total}\n")
+
+if __name__ == "__main__":
+    transform()
+
+"""
+I thought this would be nice: https://www.tutorialspoint.com/python/python_command_line_arguments.htm#:~:text=You%20can%20pass%20values%20to,argv%20variable.
+"""
